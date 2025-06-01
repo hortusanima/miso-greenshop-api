28187a3c243a91f6584af0f7d99e23d3ee8b54b8
lilacturt <jurisic.ana.official@gmail.com>
2025-06-01 16:24:25 +0200

    [Changed] CORS policy

--- miso-greenshop-api/Program.cs
+++ miso-greenshop-api/Program.cs
@@ -44,7 +44,14 @@ builder.Services
     {
         options.AddPolicy("DefaultPolicy", policy =>
             policy.WithOrigins(
-                "https://localhost:5173")
+                "https://localhost:5173",
+                "https://localhost:3000",
+                "https://127.0.0.1:5173",
+                "https://127.0.0.1:3000",
+                "http://localhost:5173",
+                "http://localhost:3000",
+                "http://127.0.0.1:5173",
+                "http://127.0.0.1:3000")
             .AllowAnyMethod()
             .AllowAnyHeader()
             .SetIsOriginAllowedToAllowWildcardSubdomains()
@@ -52,7 +59,14 @@ builder.Services
         );
         options.AddPolicy("WithCredentialsPolicy", policy =>
            policy.WithOrigins(
-               "https://localhost:5173")
+               "https://localhost:5173",
+                "https://localhost:3000",
+                "https://127.0.0.1:5173",
+                "https://127.0.0.1:3000",
+                "http://localhost:5173",
+                "http://localhost:3000",
+                "http://127.0.0.1:5173",
+                "http://127.0.0.1:3000")
            .AllowAnyMethod()
            .AllowAnyHeader()
            .AllowCredentials()
